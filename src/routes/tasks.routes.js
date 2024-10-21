c08de99cee09aae652a0b5ae43c162d90a143025
juan <juan@gmail.com>
2024-10-21 02:43:20 -0600

    TAREAS INDEPENDIENTES EN HOME

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -14,15 +14,11 @@ import { createTaskSchema } from "../schemas/task.schema.js";
 const router = Router();
 
 router.get("/tasks", getAllTasks);
-
-router.get("/tasks", auth, getTasks);
+router.get("/tasks/user", auth, getTasks);
 
 router.post("/tasks", auth, validateSchema(createTaskSchema), createTask);
-
 router.get("/tasks/:id", auth, getTask);
-
 router.put("/tasks/:id", auth, updateTask);
-
 router.delete("/tasks/:id", auth, deleteTask);
 
 export default router;
